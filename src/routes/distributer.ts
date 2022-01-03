6b991aa1f2f845af462a62c8bc40a2e76e04ad58
= <=>
2022-01-04 00:23:46 +0530

    Login Issue solved

--- src/routes/distributer.ts
+++ src/routes/distributer.ts
@@ -19,31 +19,37 @@ distributer.post("/login", async (req:Request, res:Response)=> {
   }
 
   let loginCheck:any = await Distributer.find({email: req.body.email, password: req.body.password})
- 
+
   !loginCheck ? response.message = 'This Account Does Not Exist.' : null
-  
-  if (loginCheck.email == req.body.email && loginCheck.password == req.body.password) {
-    let data:distributer = {
-      name: loginCheck.name,
-      phone: loginCheck.phone,
-      email: loginCheck.email,  
-      basePrice: loginCheck.basePrice
-    }
-    let token = jwt.sign(JSON.stringify({...data, authority:'A3'}), process.env.JWTPASS!, {
-      expiresIn: '8h'
-    })
 
-    if (loginCheck.active == false) {
-      response.message = "The account is not active at the moment, please contact your adminstrator"
-    }else {
-      response.status = true
-      response.message = 'Login Successful'
-      response.data = {
-        token : token
+
+
+    if (loginCheck[0].email == req.body.email && loginCheck[0].password == req.body.password) {
+      
+      let data:distributer = {
+        name: loginCheck[0].name,
+        phone: loginCheck[0].phone,
+        email: loginCheck[0].email,  
+        basePrice: loginCheck[0].basePrice
       }
+
+      
+      let token = jwt.sign({...data, authority:'A3'}, process.env.JWT_PASS!, {
+        expiresIn: '8h'
+      })
+  
+      if (loginCheck.active == false) {
+        response.message = "The account is not active at the moment, please contact your adminstrator"
+      }else {
+        response.status = true
+        response.message = 'Login Successful'
+        response.data = {
+          token : token
+        }
+      }
+  
     }
 
-  }
 
   res.json(response)
 })
