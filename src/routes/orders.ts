612ac436ce12e0197c8212366c1bd481b8a209cd
= <=>
2022-01-16 21:33:30 +0530

    prev

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -59,6 +59,47 @@ order.post("/place", async (req:Request, res: Response) =>{
 })
 
 
+
+
+//update order status
+order.post("/order_status", async (req:Request, res:Response)=> {
+    let response:response = {
+      status : false,
+      message : "Unable to update the order status, please try later"
+    }
+    
+    let order_id = req.body._id;
+    let status = req.body.status
+  
+    try {
+  
+  
+      if (typeof status != 'number') {
+        throw `The status must be a number, but instead recieved status of type ${typeof status}`
+      }
+      await Order.findByIdAndUpdate(order_id, {$set : {
+        status : status
+      }})
+      .then(()=>{
+        response.status = true
+        response.message = 'Order status updated successfully!'
+      })
+      .catch(()=>{throw 'somthing failed while updating the status, please try again!'})
+    } catch (error) {
+      
+      if (typeof error == 'string') {
+  
+        response.status = false
+        response.message = error
+      }
+    }
+  
+    res.json(response)
+  
+  })
+
+
+
 order.post("/list", async (req:Request, res: Response) =>{
 
     let response:response = {
